489e2f0e8cebdd65bcd106f9618b3962476edd89
root <kmicic1993@gmail.com>
2016-05-09 23:35:39 +0200

    ServersList View

--- ClientApplication/build-VirtualShutterClient-Desktop_Qt_5_6_0_GCC_64bit-Debug/ui_mainwindow.h
+++ ClientApplication/build-VirtualShutterClient-Desktop_Qt_5_6_0_GCC_64bit-Debug/ui_mainwindow.h
@@ -13,7 +13,10 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QFrame>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QListView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
@@ -35,7 +38,10 @@ public:
     QPushButton *pushButton_3;
     QPushButton *pushButton_4;
     QWidget *widget_3;
+    QLabel *label;
     QListView *listView;
+    QLineEdit *lineEdit;
+    QFrame *line;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -68,6 +74,10 @@ public:
 "selection-background-color: rgb(0, 156, 134);\n"
 "color: rgb(255, 255, 255);\n"
 "border-color: rgba(255, 255, 255, 0);"));
+        QIcon icon;
+        icon.addFile(QStringLiteral(":/res/icon-03.png"), QSize(), QIcon::Normal, QIcon::Off);
+        pushButton->setIcon(icon);
+        pushButton->setIconSize(QSize(18, 18));
         pushButton->setAutoDefault(false);
         pushButton->setFlat(false);
         pushButton_2 = new QPushButton(widget_2);
@@ -77,6 +87,10 @@ public:
 "selection-background-color: rgb(0, 156, 134);\n"
 "color: rgb(255, 255, 255);\n"
 "border-color: rgba(255, 255, 255, 0);"));
+        QIcon icon1;
+        icon1.addFile(QStringLiteral(":/res/icon-02.png"), QSize(), QIcon::Normal, QIcon::Off);
+        pushButton_2->setIcon(icon1);
+        pushButton_2->setIconSize(QSize(18, 18));
         pushButton_3 = new QPushButton(widget_2);
         pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
         pushButton_3->setGeometry(QRect(22, 120, 215, 40));
@@ -84,22 +98,59 @@ public:
 "selection-background-color: rgb(0, 156, 134);\n"
 "color: rgb(255, 255, 255);\n"
 "border-color: rgba(255, 255, 255, 0);"));
+        QIcon icon2;
+        icon2.addFile(QStringLiteral(":/res/icon-01.png"), QSize(), QIcon::Normal, QIcon::Off);
+        pushButton_3->setIcon(icon2);
+        pushButton_3->setIconSize(QSize(18, 18));
         pushButton_4 = new QPushButton(widget_2);
         pushButton_4->setObjectName(QStringLiteral("pushButton_4"));
         pushButton_4->setGeometry(QRect(0, 390, 260, 40));
+        pushButton_4->setFocusPolicy(Qt::NoFocus);
         pushButton_4->setStyleSheet(QLatin1String("background-color: rgb(0, 157, 135);\n"
 "selection-background-color: rgb(0, 156, 134);\n"
 "color: rgb(255, 255, 255);\n"
 "border-color: rgba(255, 255, 255, 0);"));
+        QIcon icon3;
+        icon3.addFile(QStringLiteral(":/images/res/connect-icon.png"), QSize(), QIcon::Normal, QIcon::Off);
+        pushButton_4->setIcon(icon3);
+        pushButton_4->setIconSize(QSize(18, 18));
+        pushButton_4->setAutoExclusive(false);
         pushButton_4->setFlat(false);
         widget_3 = new QWidget(centralWidget);
         widget_3->setObjectName(QStringLiteral("widget_3"));
-        widget_3->setGeometry(QRect(0, 65, 540, 40));
+        widget_3->setGeometry(QRect(0, 65, 540, 41));
         widget_3->setStyleSheet(QStringLiteral("background-color: rgb(221, 220, 220);"));
+        label = new QLabel(widget_3);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(10, 10, 161, 20));
+        label->setStyleSheet(QLatin1String("color: rgb(130, 131, 131);\n"
+"font: 75 13pt \"Cantarell\";"));
         listView = new QListView(centralWidget);
         listView->setObjectName(QStringLiteral("listView"));
-        listView->setGeometry(QRect(0, 105, 540, 325));
+        listView->setGeometry(QRect(0, 99, 540, 331));
+        lineEdit = new QLineEdit(centralWidget);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+        lineEdit->setGeometry(QRect(295, 15, 231, 35));
+        lineEdit->setAutoFillBackground(false);
+        lineEdit->setStyleSheet(QLatin1String("background-color: rgb(255, 255, 255);\n"
+"color: rgb(130, 131, 131);\n"
+"border-top-color: rgb(255, 255, 255);"));
+        lineEdit->setInputMask(QStringLiteral(""));
+        lineEdit->setText(QStringLiteral(""));
+        lineEdit->setFrame(false);
+        lineEdit->setAlignment(Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter);
+        line = new QFrame(centralWidget);
+        line->setObjectName(QStringLiteral("line"));
+        line->setGeometry(QRect(295, 50, 231, 5));
+        line->setFrameShape(QFrame::HLine);
+        line->setFrameShadow(QFrame::Sunken);
         MainWindow->setCentralWidget(centralWidget);
+        listView->raise();
+        widget->raise();
+        widget_2->raise();
+        widget_3->raise();
+        lineEdit->raise();
+        line->raise();
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
         menuBar->setGeometry(QRect(0, 0, 800, 26));
@@ -130,6 +181,8 @@ public:
         pushButton_2->setText(QApplication::translate("MainWindow", "REMOVE", 0));
         pushButton_3->setText(QApplication::translate("MainWindow", "EDIT", 0));
         pushButton_4->setText(QApplication::translate("MainWindow", "CONNECT", 0));
+        label->setText(QApplication::translate("MainWindow", "SERVERS LIST", 0));
+        lineEdit->setPlaceholderText(QApplication::translate("MainWindow", "TYPE NICKNAME", 0));
     } // retranslateUi
 
 };
